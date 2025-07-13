c8c7a0ba87956c4c2795976c0797bae45563c42e
Mitul1927 <mitul1927.be22@chitkara.edu.in>
2025-07-13 18:21:37 +0530

    Intercepting routes basics completed

--- intercepting_routes/src/app/ecommerce/page.tsx
+++ intercepting_routes/src/app/ecommerce/page.tsx
@@ -1,10 +1,6 @@
-const InterceptedProductOne = async ({
-  params,
-}: {
-  params: { product: string };
-}) => {
-  const { product } = await params;
+import Link from "next/link";
 
+const Ecommerce = () => {
   const products = [
     {
       id: 1,
@@ -29,26 +25,27 @@ const InterceptedProductOne = async ({
     },
   ];
 
-  const findProduct = products.find((f) => f.id === +product);
-  console.log("--------- findProduct", findProduct);
-
   return (
-    <>
-      <div className="fixed inset-0 flex items-center justify-center bg-black-200 bg-opacity-50 z-50">
-        <div className="bg-white p-8 rounded-lg shadow-lg text-center relative">
-          <h1 className="text-3xl mb-4">{findProduct?.name}</h1>
-
-          <img
-            src={findProduct?.image}
-            alt={findProduct?.name}
-            className="w-[40rem] mx-auto rounded-lg"
-          />
-
-          <p className="text-2xl mt-4">Price: {findProduct?.price}</p>
+    <div>
+      <section className="m-[4rem] ">
+        <div>
+          <h1 className="text-3xl mb-3">Ecommerce</h1>
         </div>
-      </div>
-    </>
+
+        <section className="flex gap-3 ">
+          {products.map((product) => (
+            <div className="w-[200px] border p-[1rem]" key={product.id}>
+              <Link href={`/ecommerce/${product.id}`}>
+                <img src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>{product.price}</p>
+              </Link>
+            </div>
+          ))}
+        </section>
+      </section>
+    </div>
   );
 };
 
-export default InterceptedProductOne;
+export default Ecommerce;
